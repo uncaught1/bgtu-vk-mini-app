17184d89b144497654f99d56da0b3ca6b6b155a5
UnCaughT <gosha.kolyuaev.2004@gmail.com>
2024-03-12 22:09:40 +0300

    1.2.3

--- static/js/224.8a74e460.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbgtu_vk_mini_app=self.webpackChunkbgtu_vk_mini_app||[]).push([[224],{224:(i,n,p)=>{p.r(n),p.d(n,{default:()=>a});var t=p(664),u=p.n(t);u().init();const a=u()}}]);
-//# sourceMappingURL=224.8a74e460.chunk.js.map
